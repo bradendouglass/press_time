9811c84a8d3c637b68bf8499abac932ae7ae7dbd
Braden <braden.douglass@gmail.com>
2012-08-13 15:39:02 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimePress::Application.config.secret_token = 'fc63eb7d50e0def2a73e6ae6d5f27c3d066d32576614c2171ec3bcc10d5089d2d4a2c2db5af8ccff24451e801f508330c1313acb7ea036451aea7b00adc0f878'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+TimePress::Application.routes.draw do
+  root :to => "home#index"
+end
