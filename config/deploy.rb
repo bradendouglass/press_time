ce58c9552a764fdf72c8f3f30f675a362d6cb661
Braden <braden.douglass@gmail.com>
2012-08-30 16:37:55 -0400

    initial attempt at unicorn/cap deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,150 +1,54 @@
-set :application, "Edm Press Time"
-set :repository,  "git://github.com/bradendouglass/press_time.git"
-set :branch, "origin/master"
-set :migrate_target, :current
-set :ssh_options, {:forward_agent => true}
-set :rails_env, "production"
-set :deploy_to, "/git/apps/edmpress_time"
-set :normalize_asset_timestamps, false
+require "bundler/capistrano"
 
-set :user, "deployinator"
+# Define your server here
+server "192.168.0.37", :web, :app, :db, primary: true
+
+# # Set application settings
+set :application, "press_time"
+set :user, "deployinator" # As defined on your server
 set :group, "dev"
+set :deploy_to, "/home/#{user}/apps/#{application}" # Directory in which the deployment will take place
+set :deploy_via, :remote_cache
 set :use_sudo, false
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, "git"
+set :repository, "git@github.com:bradendouglass/#{application}.git"
+set :branch, "master"
 
-role :web, "192.168.0.37"                          # Your HTTP server, Apache/etc
-role :app, "192.168.0.37"                          # This may be the same as your `Web` server
-role :db,  "192.168.0.37", :primary => true # This is where Rails migrations will run
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-set(:latest_release) { fetch(:current_path) }
-set(:release_path) { fetch(:current_path) }
-set(:current_release) { fetch(:current_path) }
-
-set(:current_revision) { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
-set(:latest_release) { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
-set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
-
-default_environment["RAILS_ENV"] = 'production'
-
-#default_environment["PATH"] = "--"
-#default_environment["GEM_HOME"] = "--"
-#default_environment["GEM_PATH"] = "--"
-default_environment["RUBY_VERSION"] = "ruby-1.9.3-p194"
-
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-  desc "Deploy your App"
-  task :default do
-    update
-    restart
-  end
-
-  desc "Setup your git-based deployment"
-  task :setup, :except => {:no_release => true} do
-    dirs = [deploy_to, shared_path]
-    dirs += shared_children.map {|d| File.join(shared_path, d)}
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-    run "git clone #{repository} #{current_path}"
-  end
-
-  task :cold do
-    update
-    migrate
-  end
-
-  task :update do
-    transaction do
-      update_code
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}" # Using unicorn as the app server
     end
   end
 
-  desc "Update the deployed code"
-  task :update_code, :except => { :no_release => true } do
-    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
-    finalize_update
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_ini.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
   end
+  after "deploy:setup", "deploy:setup_config"
 
-  desc "Update the database (overwritten to avoid symlink)"
-  task :migrations do
-    transaction do
-      update_code
-    end
-    migrate
-    restart
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
+  after "deploy:finalize_update", "deploy:symlink_config"
 
-  task :finalize_update, :except => { :no_release => true } do
-    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-
-    run <<-CMD
-    rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
-    mkdir -p #{latest_release}/public &&
-    mkdir -p #{latest_release}/tmp &&
-    ln -s #{shared_path}/log #{latest_release}/log &&
-    ln -s #{shared_path}/system #{latest_release}/public/system &&
-    ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
-    ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
-    CMD
-
-    if fetch(:normalize_asset_timestamps, true)
-      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-      asset_path = fetch(:public_children, %w(images stylesheets javascript)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
-      run "find #{asset_path} -exec touch -t #{stamp} {} ';'; true", :env => {"TZ" => "UTC"}
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
     end
   end
-
-  desc "Zero-downtime restart of Unicorn"
-  task :restart, :except => { :no_release => true } do
-    run "kill -s USR2 'cat /tmp/unicorn.my_site.pid'"
-  end
-
-  desc "Start the Unicorns"
-  task :start, :except => { :no_release => true } do
-    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
-  end
-
-  desc "Stop the Unicorns"
-  task :stop, :except => { :no_release => true } do
-    run "kill -s QUIT 'cat /tmp/unicorn.edmpress_time.pid'"
-  end
-
-  namespace :rollback do
-    desc "Moves the repo back to the previos version of HEAD"
-    task :repo, :except => { :no_release => true } do
-      set :branch, "HEAD@{1}"
-      deploy.default
-    end
-
-    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next release"
-    task :cleanup, :except => { :no_release => true } do
-      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
-    end
-
-    desc "Rolls back to the previously deployed version"
-    task :default do
-      rollback.repo
-      rollback.cleanup
-    end
-  end
+  before "deploy", "deploy:check_revision"
 end
-
-def run_rake(cmd)
-  run "cd #{current_path}; #{rake} #{cmd}"
-end
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,49 +1,24 @@
-#Environment is passed or defaults to development
-env = ENV["RAILS_ENV"] || "development"
-
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-worker_processes 4
-
-listen "/tmp/edmpress_time.socket", :backlog => 64
-
-#Preloading makes joo fast
+# Define your root directory
+root = "/home/deployer/apps/gifroll/current"
+#
+# # Define worker directory for Unicorn
+working_directory root
+#
+# # Location of PID file
+pid "#{root}/tmp/pids/unicorn.pid"
+#
+# # Define Log paths
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+#
+# # Listen on a UNIX data socket
+listen "/tmp/unicorn.gifroll.sock"
+#
+# # 16 worker processes for production environment
+worker_processes 16
+#
+# # Load rails before forking workers for better worker spawn time
 preload_app true
-
-#Nuke lazy workers faster
-timeout 30
-
-pid "/tmp/unicorn.edmpress_time.pid"
-
-if env == "production"
-  #We use current dir because cap symlinks us one. Kinda handy
-  working_directory = "/home/deployinator/apps/edmpress_time/current"
-
-  user 'deployinator', 'staff'
-  shared_path = "/home/deployinator/apps/edmpress_time/shared"
-
-  stderr_path "#{shared_path}/log/unicorn.stderr.log"
-  stdout_path "#{shared_path}/log/unicorn.stdout.log"
-end
-
-before_fork do |server,worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  #This is the magic of all that negative downtime. Kill master process
-  #that belongs to oldbin PID
-  old_pid = "/tmp/unicorn.edmpress_time.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
-
-after_fork do |server,worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
-end
+#
+# # Restart workes hangin' out for more than 240 secs
+timeout 240
