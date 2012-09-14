d79f7140c8d456c9614c008d691459b44f220e21
Braden <braden.douglass@gmail.com>
2012-09-14 11:43:35 -0400

    Unicorn init.d script needs to be givin executable rights.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
+      sudo "chmod a+x /etc/init.d/unicorn_#{application}"
       run "/etc/init.d/unicorn_#{application} #{command}" # Using unicorn as the app server
     end
   end
