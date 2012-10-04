258cb392ca8943e1d4c99e2ed9733cfee70d0abb
Braden <braden.douglass@gmail.com>
2012-10-04 10:21:15 -0400

    extracting Time logic from the views

--- app/helpers/run_stamps_helper.rb
+++ app/helpers/run_stamps_helper.rb
@@ -3,6 +3,7 @@ module RunStampsHelper
     Time.at(time).utc.strftime("%I:%M %p - %b %d, %Y")
   end
   def time_diff(stopTime, startTime)
-
+    diffTime = stopTime - startTime
+    Time.at(diffTime).utc.strftime("(%H) hours (%M) min (%S) seconds")
   end
 end
